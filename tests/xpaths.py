5252dd8c9d04f89fe6c4fe1dcd9c7b0b6b9debf4
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2014-03-29 23:52:50 +0100

    upgrade

--- tests/xpaths.py
+++ tests/xpaths.py
@@ -5,6 +5,7 @@ Created on 29 Mar 2014
 '''
 
 import unittest
+import os
 import selesame
 from selenium import webdriver
 
@@ -13,15 +14,48 @@ class Test(unittest.TestCase):
 		self.driver = webdriver.Firefox()
 
 	def tearDown(self):
-		#self.driver.close()
-		pass
+		self.driver.close()
 
-	def testXPath(self):
+	@classmethod
+	def setUpClass(cls):
+		path = os.getcwd().split(os.sep)
+
+		for d in reversed(path[:]):
+			if d != 'selesame':
+				path.pop()
+				continue
+
+			break
+
+		path.append("tests")
+		path.append("www")
+
+		cls.path = "file://" + os.sep.join(path)
+
+	def getUrl(self, filename):
+		return self.path + os.sep + filename
+
+	def testCorrectnesOfXPaths(self):
 		d = selesame.analyze(url = "http://www.kinyen.pl/", driver = self.driver)
 
 		for v in d.itervalues():
 			for xpath in v:
 				self.assertIsNotNone(self.driver.find_element_by_xpath(xpath))
+
+	def testNumberOfLinksOnPage(self):
+		d = selesame.analyze(url = self.getUrl("xpaths.html"), driver = self.driver)
+
+		self.assertEqual(3, len(d['http://example.com/']))
+
+	def testNumberOfLinksForId(self):
+		d = selesame.get_same(url = self.getUrl("xpaths.html"), id = "a", driver = self.driver)
+
+		self.assertEqual(3, len(d))
+
+	def testNumberOfLinksForXPath(self):
+		d = selesame.get_same(url = self.getUrl("xpaths.html"), xpath = '//*[@id="c"]', driver = self.driver)
+
+		self.assertEqual(3, len(d))
 
 if __name__ == "__main__":
 	#import sys;sys.argv = ['', 'Test.testName']
